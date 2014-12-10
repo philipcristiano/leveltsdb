d4139dec6c7e08dc123b69eb4bddd2b3de77001c
Philip Cristiano <philipcristiano@spotify.com>
2014-12-10 10:21:08 -0500

    Test reading multiple

--- test/leveltsdb_SUITE.erl
+++ test/leveltsdb_SUITE.erl
@@ -2,13 +2,13 @@
 -include_lib("common_test/include/ct.hrl").
 -export([all/0]).
 -export([groups/0, init_per_group/2, end_per_group/2]).
--export([write_read/1]).
+-export([write_read/1, write_multiple/1]).
 
 all() -> [{group, db}].
 
 groups() -> [{db,
-             [parallel, {repeat, 10}],
-             [write_read]}].
+             [shuffle],
+             [write_read, write_multiple]}].
 
 init_per_group(_, Config) ->
     Config.
@@ -22,5 +22,16 @@ write_read(Config) ->
     {K, V} = {<<"Key">>, <<"Value">>},
     TS = 1418223408,
     leveltsdb:write(DB, K, TS, V),
-    {ok, New_V} = leveltsdb:get(DB, K, TS),
-    New_V = V.
+    {ok, Read_V} = leveltsdb:get(DB, K, TS),
+    Read_V = V.
+
+write_multiple(Config) ->
+    Dir = ?config(priv_dir, Config),
+    {ok, DB} = leveltsdb:open(Dir),
+    {K, V} = {<<"Key">>, <<"Value">>},
+    TS = 1418223408,
+    leveltsdb:write(DB, K, 1418223407, V),
+    leveltsdb:write(DB, K, TS, V),
+    leveltsdb:write(DB, K, 1418223409, V),
+    {ok, Read_V} = leveltsdb:get(DB, K, TS),
+    Read_V = V.
