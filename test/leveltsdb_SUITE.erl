c4095c43f9c6131337bd513df67d17d301931c57
Philip Cristiano <philipcristiano@spotify.com>
2014-12-10 11:38:21 -0500

    tests: Cleanup, use assertions

--- test/leveltsdb_SUITE.erl
+++ test/leveltsdb_SUITE.erl
@@ -1,5 +1,7 @@
 -module(leveltsdb_SUITE).
 -include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
 -export([all/0]).
 -export([groups/0, init_per_group/2, end_per_group/2]).
 -export([write_read/1, write_multiple/1]).
@@ -16,22 +18,25 @@ init_per_group(_, Config) ->
 end_per_group(_, _Config) ->
     ok.
 
-write_read(Config) ->
+db_for_config(Config) ->
     Dir = ?config(priv_dir, Config),
     {ok, DB} = leveltsdb:open(Dir),
+    DB.
+
+write_read(Config) ->
+    DB = db_for_config(Config),
     {K, V} = {<<"Key">>, <<"Value">>},
     TS = 1418223408,
     leveltsdb:write(DB, K, TS, V),
     {ok, Read_V} = leveltsdb:get(DB, K, TS),
-    Read_V = V.
+    ?assertEqual(V, Read_V).
 
 write_multiple(Config) ->
-    Dir = ?config(priv_dir, Config),
-    {ok, DB} = leveltsdb:open(Dir),
+    DB = db_for_config(Config),
     {K, V} = {<<"Key">>, <<"Value">>},
     TS = 1418223408,
     leveltsdb:write(DB, K, 1418223407, V),
     leveltsdb:write(DB, K, TS, V),
     leveltsdb:write(DB, K, 1418223409, V),
     {ok, Read_V} = leveltsdb:get(DB, K, TS),
-    Read_V = V.
+    ?assertEqual(V, Read_V).
